44c511c79c53b39a4d27f416a1817b2322d62141
Tarek Gabarin <tarekgabarin1992@gmail.com>
2017-08-30 14:41:17 -0400

    Commited

--- config/config.js
+++ config/config.js
@@ -3,5 +3,5 @@
 
 module.exports = {
     'secretKey': '54323-19234-96844-23453',
-    'mongoUrl' : 'mongodb://TG1992:sanctum@ds159033.mlab.com:59033/deelish'
+    'mongoUrl' : process.env.MONGODB_URI
 };
