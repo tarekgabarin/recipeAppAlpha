4c018e6b680b566cb6477d403ff3594688868e55
Tarek Gabarin <tarekgabarin1992@gmail.com>
2018-01-05 15:01:30 -0500

    cors should be fixed

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -60,7 +60,7 @@ const upload = multer({
 
 router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth, X-Auth");
     next();
 });
 
